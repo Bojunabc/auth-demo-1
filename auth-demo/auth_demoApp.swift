76c285a26a85431043883bdff275f7149e97f370
Alan <a0928033930@gmail.com>
2022-12-09 10:06:09 +0800

    Initial Commit

---
+++ auth-demo/auth_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  auth_demoApp.swift
+//  auth-demo
+//
+//  Created by 暨大附中 on 2022/12/9.
+//
+
+import SwiftUI
+
+@main
+struct auth_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
